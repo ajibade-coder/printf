225afa23975ffe80936828b4da7cff306a7ac11d
ajibad-coder <meriesbells1996@gmail.com>
2023-08-22 11:10:38 -0700

    update

--- _printf.c
+++ _printf.c
@@ -6,36 +6,36 @@
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	int index;
-	va_list arguements;
+        int len = 0;
+        int index;
+        va_list arguements;
 
-	if (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	va_start(arguements, format);
+        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+                return (-1);
+        if (!format || (format[0] == '%' && !format[1]))
+                return (-1);
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
+                return (-1);
+        va_start(arguements, format);
 
-	for (index = 0 ; format[index] != '\0' ; index++)
-	{
-		if (format[index] != '%')
-			myputchar(format[index]);
-		else
-		{
-			if (format[index + 1] == 'c')
-				c_specifier(va_arg(arguements, int));
-			else if (format[index + 1] == 's')
-				len += (str_specifier(va_arg(arguements, char *)) - 1);
-			else if (format[index + 1] == '%')
-				percent_spcfr();
-			else if (format[index + 1] == 'd' || format[index + 1] == 'i')
-				len += (print_integer(va_arg(arguements, int)) - 1);
-			index++;
-		}
-		len++;
-	}
-	va_end(arguements);
+        for (index = 0 ; format[index] != '\0' ; index++)
+        {
+                if (format[index] != '%')
+                        myputchar(format[index]);
+                else
+                {
+                        if (format[index + 1] == 'c')
+                                c_specifier(va_arg(arguements, int));
+                        else if (format[index + 1] == 's')
+                                len += (str_specifier(va_arg(arguements, char *)) - 1);
+                        else if (format[index + 1] == '%')
+                                percent_spcfr();
+                        else if (format[index + 1] == 'd' || format[index + 1] == 'i')
+                                len += (print_integer(va_arg(arguements, int)) - 1);
+                        index++;
+                }
+                len++;
+        }
+        va_end(arguements);
 	return (len);
 }
